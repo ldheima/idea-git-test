5821d854e10b7678de9ec66a791fe96f2adf8495
heimaTest <1256101731@qq.com>
2021-11-03 11:44:13 +0800

    Revert "第二次使用idea提交代码"
    This reverts commit f1c63766

--- itheima-git-test/src/com/itheima/git/HelloWorld.java
+++ itheima-git-test/src/com/itheima/git/HelloWorld.java
@@ -4,6 +4,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("helloworld");
-        System.out.println("helloworld");
     }
 }
